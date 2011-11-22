2c867c2ef0c6c6ad6cf8a2622ef6935c8446c2fa
Tiago Proenca <tproenca@gmail.com>
2011-11-22 14:05:39 -0800

    Updated the view code.

--- extensions/LighthouseCodeReview/src/edu/uci/lighthouse/extensions/codereview/ui/ReviewRequestsView.java
+++ extensions/LighthouseCodeReview/src/edu/uci/lighthouse/extensions/codereview/ui/ReviewRequestsView.java
@@ -1,183 +1,169 @@
 package edu.uci.lighthouse.extensions.codereview.ui;
 
-import java.util.Date;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
 
-import javax.persistence.EntityManager;
-
-import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.preference.JFacePreferences;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 import edu.uci.lighthouse.extensions.codereview.model.CodeReview;
+import edu.uci.lighthouse.extensions.codereview.model.CodeReviewModel;
 import edu.uci.lighthouse.extensions.codereview.model.CodeSelection;
 import edu.uci.lighthouse.extensions.codereview.model.Comment;
-import edu.uci.lighthouse.extensions.codereview.model.FileSnapshot;
-import edu.uci.lighthouse.model.LighthouseAuthor;
-import edu.uci.lighthouse.model.jpa.AbstractDAO;
-import edu.uci.lighthouse.model.jpa.JPAException;
-import edu.uci.lighthouse.model.jpa.JPAUtility;
-import edu.uci.lighthouse.model.jpa.LHAuthorDAO;
+import edu.uci.lighthouse.extensions.codereview.model.ICodeReviewModelListener;
 
 /**
  * View that shows all requests for review.
  * 
  * @author Tiago Proenca (tproenca@gmail.com)
  */
-public class ReviewRequestsView extends ViewPart {
-
-	private TableViewer viewer;
-
+public class ReviewRequestsView extends ViewPart implements ICodeReviewModelListener, ISelectionChangedListener{
+	
+//	private StyledText chatHistory;
+	private CommentHistory chatHistory;
+	private Text chatMessage;
+	private TreeViewer viewer;
+	private CodeReviewModel model = CodeReviewModel.getInstance();
+	
 	@Override
 	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
-				| SWT.V_SCROLL);
-		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewLabelProvider());
-		// viewer.setSorter(new NameSorter());
-		viewer.setInput(getViewSite());
+
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
+		viewer.setContentProvider(new CodeReviewContentProvider());
+		viewer.setLabelProvider(new CodeReviewLabelProvider());
+		viewer.setSorter(new ViewerSorter());
+		viewer.setInput(model);
+		viewer.addSelectionChangedListener(this);
 		
-//		AbstractNotification n = new AbstractNotification(){
-
-//		Notifications.getService().notify(  
-//				  Collections.singletonList(n)) ; 
-		RequestNotification n = new RequestNotification(parent.getDisplay());
-		n.create();
-		n.open();
+		Composite composite = new Composite(parent, SWT.NULL);
+		//composite.setBackground(ColorConstants.red);
+		composite.setLayout(new FormLayout());
+		FormData fData;
 		
-		try {
-			List<LighthouseAuthor> authors = new LHAuthorDAO().list();
-			Comment comment = new Comment(authors.get(0), "comment1");
-//			AbstractDAO<Comment, Date> commentDAO = new AbstractDAO<Comment, Date>(){};
-//			Comment comment2 =  commentDAO.save(comment);
-			
-//			EntityManager entityManager = JPAUtility.createEntityManager();
-//			JPAUtility.beginTransaction(entityManager);
-//			entityManager.persist(comment);
-//			JPAUtility.commitTransaction(entityManager);
-////			JPAUtility.closeEntityManager(entityManager);
-//			System.out.println("Datetime:"+comment.getTimestamp()+" Id:"+comment.getId());
-//			comment.setContent("Tiago");
-////			entityManager = JPAUtility.createEntityManager();
-//			JPAUtility.beginTransaction(entityManager);
-////			comment = entityManager.find(Comment.class, comment.getId());
-////			entityManager.merge(comment);
-//			entityManager.refresh(comment);
-//			JPAUtility.commitTransaction(entityManager);
-//			JPAUtility.closeEntityManager(entityManager);			
-//			
-//			System.out.println("Datetime:"+comment.getTimestamp()+" Id:"+comment.getId());
-			
-			ITextSelection selection = new ITextSelection(){
-				@Override
-				public boolean isEmpty() {
-					// TODO Auto-generated method stub
-					return false;
-				}
-
-				@Override
-				public int getOffset() {
-					// TODO Auto-generated method stub
-					return 1;
-				}
-
-				@Override
-				public int getLength() {
-					// TODO Auto-generated method stub
-					return 5;
-				}
-
-				@Override
-				public int getStartLine() {
-					// TODO Auto-generated method stub
-					return 0;
-				}
-
-				@Override
-				public int getEndLine() {
-					// TODO Auto-generated method stub
-					return 0;
-				}
-
-				@Override
-				public String getText() {
-					// TODO Auto-generated method stub
-					return "";
-				}
-			};
-			
-			CodeSelection codeSelection = new CodeSelection(selection);
-			codeSelection.addComment(comment);
-			
-			FileSnapshot fs = new FileSnapshot();
-			fs.addCodeSelection(codeSelection);
-			fs.setContent("File Content");
-			
-			CodeReview creview = new CodeReview(authors.get(0));
-			creview.setReviewer(authors.get(0));
-			creview.addFileSnapshot(fs);
-			
-			
-			EntityManager entityManager = JPAUtility.createEntityManager();
-			JPAUtility.beginTransaction(entityManager);
-			entityManager.persist(creview);
-			JPAUtility.commitTransaction(entityManager);
-			JPAUtility.closeEntityManager(entityManager);
-			
-		} catch (JPAException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		fData = new FormData();
+		fData.top = new FormAttachment(0,0);
+		fData.left = new FormAttachment(0,0);
+		fData.right = new FormAttachment(100,0);
+		fData.bottom = new FormAttachment(80,0);
+//		chatHistory = new StyledText(composite, SWT.MULTI | SWT.V_SCROLL | SWT.BORDER);
+//		chatHistory.setEditable(false);
+		chatHistory = new CommentHistory(composite, SWT.BORDER);
+		chatHistory.setLayoutData(fData);
 		
-		//Comment comment = new Comment();
+//		URL url = getClass().getResource("/chat.css");
 		
 		
+//		StringBuffer buffer = new StringBuffer();
+//		buffer.append("<style type=\"text/css\" >");
+////		buffer.append("<style type=\"text/css\" src=\""+url.toString()+"\" />");
+//		//getClass().getResourceAsStream("/chat.css")
+//		BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/chat.css")));
+//		String line;
+//		try {
+//			while((line = br.readLine()) != null){
+//				buffer.append(line);
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		buffer.append("</style>");
+//		chatHistory.setText(buffer.toString());
+
+		fData = new FormData();
+		fData.top = new FormAttachment(chatHistory,0);
+		fData.left = new FormAttachment(0,0);
+		fData.right = new FormAttachment(100,0);
+		fData.bottom = new FormAttachment(100,0);
+		chatMessage = new Text(composite, SWT.BORDER);
+		chatMessage.setLayoutData(fData);		
 		
+		model.addCodeReviewModelListener(this);
 	}
 
 	@Override
 	public void setFocus() {
-		viewer.getControl().setFocus();
+		//viewer.getControl().setFocus();
+	}
+	
+	@Override
+	public void added(Collection<CodeReview> reviews) {
+		refreshView();
 	}
 
-	private static class ViewContentProvider implements
-			IStructuredContentProvider {
-		@Override
-		public void dispose() {
-		}
-
-		@Override
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		}
-
-		@Override
-		public Object[] getElements(Object inputElement) {
-			// TODO (tproenca): Return the model objects here. Model objects
-			// should contain name, timestamp, comment, fqn.
-			return new Object[]{""};
-		}
+	@Override
+	public void changed(Collection<CodeReview> reviews) {
+		refreshView();
 	}
 
-	private static class ViewLabelProvider extends LabelProvider implements
-			ITableLabelProvider {
-		@Override
-		public Image getColumnImage(Object element, int columnIndex) {
-			return null;
-		}
-
-		@Override
-		public String getColumnText(Object element, int columnIndex) {
-			// TODO (tproenca): Return the labels here
-			return "";
-		}
-
+	@Override
+	public void removed(Collection<CodeReview> reviews) {
+		refreshView();
+	}
+	
+	private void refreshView(){
+		Display.getDefault().asyncExec(new Runnable(){
+			@Override
+			public void run() {
+				viewer.refresh();				
+			}
+		});
+	}
+	
+	@Override
+	public void dispose() {
+		model.removeCodeReviewModelListener(this);
+		super.dispose();
 	}
 
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+//		chatHistory.setText("");
+		
+		TreeSelection ts = (TreeSelection)event.getSelection();
+		if (ts.getFirstElement() instanceof CodeSelection) {
+			CodeSelection selection = (CodeSelection) ts.getFirstElement();
+//			Color color = JFaceResources.getColorRegistry().get(JFacePreferences.COUNTER_COLOR);
+//			StringBuffer buffer = new StringBuffer(chatHistory.getText());
+//			for (Comment c: selection.getComments()){
+//				chatHistory.addComment(c);
+//				buffer.append(getHTMLText(c));
+//				String styledText = c.getAuthor().getName()+": ";
+//				StyleRange style = new StyleRange();
+//				style.start = chatHistory.getCharCount();
+//				style.length = styledText.length();
+//				style.fontStyle = SWT.BOLD;
+//				style.foreground = color;
+//				chatHistory.append(styledText+c.getContent()+"\n");
+//				chatHistory.setStyleRange(style);
+			chatHistory.setComments(selection.getComments());
+			}
+//			chatHistory.setText(buffer.toString());
+		
+//		}
+		
+	}
+	
+	private String getHTMLText(Comment c){
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<span class=\"author\">"+c.getAuthor().getName()+"</span>");
+		buffer.append("<P>"+c.getContent()+"</P>");
+		return buffer.toString();
+	}
 }
