4d2935fab4afccec65984915c8f0eeec93c461f5
Tiago Proenca <tproenca@gmail.com>
2011-11-22 14:33:30 -0800

    Added ICodeReviewImages file.

---
+++ extensions/LighthouseCodeReview/src/edu/uci/lighthouse/extensions/codereview/ui/ICodeReviewImages.java
@@ -0,0 +1,8 @@
+package edu.uci.lighthouse.extensions.codereview.ui;
+
+public interface ICodeReviewImages {
+
+	public final String IMG_REVIEW = "codereview-review";
+	public final String IMG_SELECTION = "codereview-selection";
+
+}
